3e85c13e4fa813fcb45c2da1dbf9e042cc4defa4
Azzam Azhari <azzamazhari.dev@gmail.com>
2025-02-03 23:01:08 +0700

    first

---
+++ app/layout.jsx
@@ -0,0 +1,10 @@
+export default function layout({ children }) {
+    return (
+      <html lang="en">
+        <head>
+          <title>Next.Js</title>
+        </head>
+        <body>{children}</body>
+      </html>
+    );
+  }
