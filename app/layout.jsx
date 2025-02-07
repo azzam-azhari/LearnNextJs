464ec0a5b1d01e086f1013a2e0bd849e5b383506
Azzam Azhari <azzamazhari.dev@gmail.com>
2025-02-07 12:53:08 +0700

    styling: header & footer

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,9 +5,9 @@ export default function layout({ children }) {
         <title>Next.Js</title>
       </head>
       <body>
-        <header style={{ border: '1px solid black' }}>[Header]</header>
+        <header style={{ border: '1px solid black', padding: '10px' }}>[Header]</header>
         <main>{children}</main>
-        <footer style={{ border: '1px solid black' }}>[Footer]</footer>
+        <footer style={{ border: '1px solid black', padding: '10px' }}>[Footer]</footer>
       </body>
     </html>
   );
